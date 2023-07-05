075c8051dedd32087e76d0b59035b1a0549f5522
zqjpeter <zqjpeter@126.com>
2023-07-05 17:29:42 +0800

    添加管理员编辑题目界面的markdown编辑器

--- beatcode-frontend/src/components/AdminScene/EditSingleProblem.js
+++ beatcode-frontend/src/components/AdminScene/EditSingleProblem.js
@@ -1,12 +1,32 @@
 import React from 'react';
+import SimpleMDEEditor from 'react-simplemde-editor';
+import 'easymde/dist/easymde.min.css';
 
 /**
  * @Description : 编辑单个题目（管理员）
  * */
 function EditSingleProblem() {
+    const options = {
+        autofocus: true, // 自动聚焦
+        hideIcons: ['preview', 'side-by-side'], // 隐藏预览、并排按钮
+    }
     return (
         <div>
             <h1>EditSingleProblem</h1>
+            <div
+                style={{
+                    marginLeft: 60,
+                    marginRight: 60,
+                    marginBottom: 30,
+
+                }}
+            >
+                <SimpleMDEEditor
+                    // 设置props
+                    options={options}
+                />
+            </div>
+
         </div>
     );
 }
