7806075760e86f1d4e49c3b8fd13ba304ae37a00
zqjpeter <zqjpeter@126.com>
2023-07-11 16:48:46 +0800

    loading

--- beatcode-frontend/src/components/MainScene/AllSubmissions.js
+++ beatcode-frontend/src/components/MainScene/AllSubmissions.js
@@ -1,12 +1,12 @@
 import React, {useEffect, useState} from "react";
-import {Button, Col, Row, Table} from "antd";
+import {Button, Col, Row, Spin, Table} from "antd";
 import {NavLink} from "react-router-dom";
-import { PieChart, Pie, Cell, Legend } from "recharts";
+import {PieChart, Pie, Cell, Legend} from "recharts";
 import {getSubmissions} from "../../services/submissionService";
 import Search from "antd/es/input/Search";
 
-import { Input, Select, Space } from 'antd';
-import { SearchOutlined } from '@ant-design/icons';
+import {Input, Select, Space} from 'antd';
+import {SearchOutlined} from '@ant-design/icons';
 
 /**
  * @Description: 某个用户所有的提交
@@ -18,29 +18,38 @@ import { SearchOutlined } from '@ant-design/icons';
  * */
 function AllSubmissions() {
 
-    const [submissions,setSubmissions]=useState([]);
-    const getCallback=(data)=>{
+    const [searchText1, setSearchText1] = useState('');
+    const [searchText2, setSearchText2] = useState('');
+    const [searchText3, setSearchText3] = useState('');
+    // 在没加载完数据之前，不显示表格
+    const [tableLoading, setTableLoading] = useState(true);
+
+    const [submissions, setSubmissions] = useState([]);
+    const getCallback = (data) => {
         console.log(data.content);
         console.log(JSON.stringify(data.content[0]._id));
         const extractedSubmissions = data.content.map((item) => ({
-            _id:item.string_id,
-            key:item.string_id,
-            problem_id:item.problemId,
-            problem_name:item.problemName,
-            user_name:item.userName,
-            result_score:item.result_score,
-            result_time:item.result_time,
-            result_memory:item.result_memory,
-            submission_time:item.submission_time,
+            _id: item.string_id,
+            key: item.string_id,
+            problem_id: item.problemId,
+            problem_name: item.problemName,
+            user_name: item.userName,
+            result_score: item.result_score,
+            result_time: item.result_time,
+            result_memory: item.result_memory,
+            submission_time: item.submission_time,
         }));
         setSubmissions(extractedSubmissions);
+
+        // 已经加载完成，设置为false
+        setTableLoading(false);
     }
-    useEffect(()=>{
+    useEffect(() => {
         getSubmissions({
-            page:"1",
-            pageSize:"50",
-        },getCallback);
-    },[])
+            page: "1",
+            pageSize: "50",
+        }, getCallback);
+    }, [])
 
 
     // const submissions = [
@@ -72,7 +81,8 @@ function AllSubmissions() {
         // 向后端发送请求，更新前端的搜索结果
         /*
         * 请求中需要包含的参数：
-        *  //needing params:
+        *
+        //needing params:
         //sortDirection: asc or desc(default)
         //sortBy: sorted field (set time as default)
         //user_name: filtered username
@@ -81,105 +91,165 @@ function AllSubmissions() {
         //page: present page
         //pageSize: size of per page
         * */
+        getSubmissions({
+            // 新搜索的话，页数一定是1
+            page: "1",
+            pageSize: "50",
+
+            // 这三个都是搜索框里的东西
+            user_name: searchText3,
+            problem_name: searchText2,
+            problem_id: searchText1,
+
+
+        }, getCallback);
     };
 
+    // TODO: const handlePageChange
 
-  return (
-      <div>
-        <div
+    if (tableLoading) {
+        return <div
             style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
                 marginTop: 20,
                 marginLeft: 20,
                 marginRight: 20,
+                marginBottom: 20,
             }}
         >
-          <div style={{height: 20,}}/>
-            {/*占位*/}
-            <div style={{height: 20,}}/>
-            {/*搜索框居中*/}
+            {/*spin居中*/}
+            <Spin
+                tip="Loading..."
+                size={"large"}
+                style={{
+                    marginTop: 20,
+                    marginLeft: 20,
+                    marginRight: 20,
+                }}
+            />
+            <div>
+                loading...
+            </div>
+            <div style={{height: 500,}}/>
+        </div>;
+    }
+
+    return (
+        <div>
             <div
                 style={{
+                    marginTop: 20,
+                    marginLeft: 20,
+                    marginRight: 20,
+                }}
+            >
+                <div style={{height: 20,}}/>
+                {/*占位*/}
+                <div style={{height: 20,}}/>
+                {/*搜索框居中*/}
+                <div
+                    style={{
                         display: "flex",
                         justifyContent: "center",
                         alignItems: "center",
                         marginBottom: 30,
-                }}
-            >
-                <Space.Compact size="large">
-                    <Input placeholder="输入题目id"  />
-                    <Input placeholder="输入题目名称"  />
-                    <Input placeholder="输入用户名"  />
-                    <Button type="primary" onClick={onSearch}>搜索</Button>
-                </Space.Compact>
+                    }}
+                >
+                    <Space.Compact size="large">
+                        <Input
+                            placeholder="输入题目id"
+                            value={searchText1}
+                            onChange={(e) => setSearchText1(e.target.value)}
+                        />
+                        <Input
+                            placeholder="输入题目名称"
+                            value={searchText2}
+                            onChange={(e) => setSearchText2(e.target.value)}
+                        />
+                        <Input
+                            placeholder="输入用户名"
+                            value={searchText3}
+                            onChange={(e) => setSearchText3(e.target.value)}
+                        />
+                        <Button type="primary" onClick={onSearch}>搜索</Button>
+                    </Space.Compact>
+                </div>
+                {/*pageSize是50*/}
+                <Table dataSource={submissions}
+                       pagination={{pageSize: 50}}
+
+                       style={{
+                           marginLeft: 40,
+                           marginRight: 40,
+                       }}
+
+                       columns={[
+                           // 单击提交id，可以跳转到提交详情界面
+                           {title: '提交时间', dataIndex: 'submission_time'},
+                           {
+                               title: '题目id',
+                               dataIndex: 'problem_id',
+                               // 渲染一个链接，单击后跳到题目详情
+                               render: (text, record) => (
+                                   <NavLink to={`/problem/${record.problem_id}`}>
+                                       {text}
+                                   </NavLink>)
+                           },
+                           {
+                               title: '题目名称',
+                               dataIndex: 'problem_name',
+                               // 渲染一个链接，单击后根据题号跳到题目详情
+                               render: (text, record) => (
+                                   <NavLink to={`/problem/${record.problem_id}`}>
+                                       {text}
+                                   </NavLink>)
+                           },
+                           {title: '用户名', dataIndex: 'user_name'},
+                           // 除了Accepted，其他的都是红色，加粗；Accepted是绿色，加粗
+                           {
+                               title: '通过状态',
+                               dataIndex: 'result_score',
+                               render: (text, record) => (
+                                   <span style={{
+                                       color: text === "100" ? "lightgreen" : "red",
+                                       fontWeight: "bold"
+                                   }}>{text}</span>
+                               )
+                           },
+                           {title: '得分', dataIndex: 'result_score'},
+                           {
+                               title: '运行时间', dataIndex: 'result_time',
+                               render: (text, record) => (
+                                   <span>{text} ms</span>
+                               )
+                           },
+                           {
+                               title: '运行内存', dataIndex: 'result_memory',
+                               render: (text, record) => (
+                                   <span>{text} MB</span>
+                               )
+                           },
+                           {
+                               title: '详情',
+                               dataIndex: '_id',
+                               // 渲染一个按钮，点击按钮跳转到提交详情界面
+                               render: (text, record) => (
+                                   <NavLink to={`/submission/${record._id}`}>
+                                       <Button>
+                                           详情
+                                       </Button>
+                                   </NavLink>)
+                           },
+                       ]}
+
+                />
+                {/*占位*/}
+                <div style={{height: 50,}}/>
             </div>
-            {/*pageSize是20*/}
-            <Table dataSource={submissions}
-                     pagination={{ pageSize: 20 }}
-
-                        style={{
-                            marginLeft: 40,
-                            marginRight: 40,
-                        }}
-
-                        columns={[
-                            // 单击提交id，可以跳转到提交详情界面
-                            { title: '提交时间', dataIndex: 'submission_time' },
-                            {
-                                title: '题目id',
-                                dataIndex: 'problem_id' ,
-                                // 渲染一个链接，单击后跳到题目详情
-                                render: (text, record) => (
-                                    <NavLink to={`/problem/${record.problem_id}`}>
-                                        {text}
-                                    </NavLink>)
-                            },
-                            {
-                                title: '题目名称',
-                                dataIndex: 'problem_name' ,
-                                // 渲染一个链接，单击后根据题号跳到题目详情
-                                render: (text, record) => (
-                                    <NavLink to={`/problem/${record.problem_id}`}>
-                                        {text}
-                                    </NavLink>)
-                            },
-                            { title: '用户名', dataIndex: 'user_name' },
-                            // 除了Accepted，其他的都是红色，加粗；Accepted是绿色，加粗
-                            {
-                                title: '通过状态',
-                                dataIndex: 'result_score' ,
-                                render: (text, record) => (
-                                    <span style={{color: text === "100" ? "lightgreen" : "red", fontWeight: "bold"}}>{text}</span>
-                                )
-                            },
-                            { title: '得分', dataIndex: 'result_score' },
-                            { title: '运行时间', dataIndex: 'result_time' ,
-                                render: (text, record) => (
-                                    <span>{text} ms</span>
-                                )
-                            },
-                            { title: '运行内存', dataIndex: 'result_memory' ,
-                                render: (text, record) => (
-                                    <span>{text} MB</span>
-                                )},
-                            {
-                                title: '详情',
-                                dataIndex: '_id' ,
-                                // 渲染一个按钮，点击按钮跳转到提交详情界面
-                                render: (text, record) => (
-                                    <NavLink to={`/submission/${record._id}`}>
-                                        <Button>
-                                            详情
-                                        </Button>
-                                    </NavLink>)
-                            },
-                        ]}
-
-            />
-            {/*占位*/}
-            <div style={{height: 50,}}/>
         </div>
-      </div>
-  );
+    );
 }
 
 export default AllSubmissions;
